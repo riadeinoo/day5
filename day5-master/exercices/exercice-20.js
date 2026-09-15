f2b809546686bfa60040392ea652e478bf65087a
riadeinoo <fakeriadeino@gmail.com>
2026-09-15 10:53:15 +0200

    saf

--- day5-master/exercices/exercice-20.js
+++ day5-master/exercices/exercice-20.js
@@ -16,4 +16,7 @@
 
 // 1. Identifie les données nécessaires.
 // 2. Écris ta solution sous cette ligne.
-// TODO: écris ta solution ici.
+let url = "Les 10 secrets de JavaScript !"
+let newurl = url.toLowerCase().replaceAll("!","").replaceAll("?","").trim().replaceAll(" ","-")
+
+console.log(newurl)
